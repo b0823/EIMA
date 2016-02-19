07233f094100999401e3b75b621bf62d1c91cd3e
brad reynolds <reynolds.0345@gmail.com>
2016-02-19 01:00:36 -0500

    Fixed some stuff with toolbar, added hybrid view and created functions for TBIs to call
    added some comments on what might be best solutions

--- App/EIMA Init/EIMA/EIMA Shared/Pages/MapPage.cs
+++ App/EIMA Init/EIMA/EIMA Shared/Pages/MapPage.cs
@@ -27,7 +27,7 @@ namespace EIMAMaster
 			var stack = new StackLayout { Spacing = 0 };
 			stack.Children.Add(map);
 			Content = stack;
-			Title = "Map";
+			Title = "Maps";
 			Icon = "Map.png";
 
 			getLocation ();
@@ -38,19 +38,20 @@ namespace EIMAMaster
 			ToolbarItem plusTBI = null;
 			ToolbarItem refreshTBI = null;
 			ToolbarItem filterTBI = null;
+			ToolbarItem mapTypeTBI = null;
 
-			if (Device.OS == TargetPlatform.Android) {
-				plusTBI = new ToolbarItem ("", "", () => {
-				}, 0, 0);
-				refreshTBI = new ToolbarItem ("", "", () => {
-				}, 0, 0);
-				filterTBI = new ToolbarItem ("", "", () => {
-				}, 0, 0);
-			}
+			plusTBI = new ToolbarItem ("", "", () => {addToMap();}, 0, 0);
+			refreshTBI = new ToolbarItem ("", "", () => {refreshData();}, 0, 0);
+			filterTBI = new ToolbarItem ("", "", () => {filterMapItems();}, 0, 0);
+			mapTypeTBI = new ToolbarItem ("", "", () => {changeMap();}, 0, 0);
+
 			plusTBI.Icon = "Plus.png";
 			refreshTBI.Icon = "Refresh.png";
 			filterTBI.Icon = "Filter.png";
+			mapTypeTBI.Icon = "MapChange.png";
 
+			//Change map type
+			ToolbarItems.Add (mapTypeTBI);
 			//refresh won't be present for stdAloneUser
 			ToolbarItems.Add (refreshTBI);
 			ToolbarItems.Add (filterTBI);
@@ -58,6 +59,27 @@ namespace EIMAMaster
 			ToolbarItems.Add (plusTBI);
 
 		}
+
+		public void addToMap(){
+			//https://forums.xamarin.com/discussion/39024/detect-a-tap-and-add-a-pin-to-the-map
+		}
+
+		public void filterMapItems(){
+			//Easiest might be a pop up with a select what you want.. OK/CANCEL
+			//https://developer.xamarin.com/guides/xamarin-forms/user-interface/navigation/pop-ups/
+		}
+
+		public void refreshData(){
+			//This won't be implemented for a while.
+		}
+
+		public void changeMap(){
+			if (map.MapType == MapType.Hybrid) {
+				map.MapType = MapType.Street;
+			} else
+				map.MapType = MapType.Hybrid;
+		}
+
 		public async void getLocation(){
 			var position = await CrossGeolocator.Current.GetPositionAsync (timeoutMilliseconds: 15000);
 			Position parsed = new Position(position.Latitude,position.Longitude);
