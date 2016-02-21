b0e9bca050cfcb817456fe250b3938624eb332a4
brad reynolds <reynolds.0345@gmail.com>
2016-02-21 03:02:00 -0500

    Big Push Big Message
    Basically I went through the process of learning about map touching
    interactions within Xam Forms
    
    tl;dr : libraries make things easier and not dependency service based
    (Good). https://github.com/TorbenK/TK.CustomMap Is what I used. It’s a
    nuget package and is packages of project now.
    
    It supports touching pins for more data, custom pins, adding pins via
    long presses, and moving pins. It’s an extension of Xamarin forms Maps
    so everything should work almost the exact same.
    
    Also added the Select what you want which is another block of code but
    its pretty straightforward.
    
    I’m putting this on a different branch so if robert wants to push code
    it isn’t impossible to understand what I did to main because there are
    at surface level lots of changes, I’ll integrate after Tuesday.

---
+++ App/EIMA Init/EIMA/EIMA Shared/Pages/EIMAMap/AddToMapPage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace EIMAMaster
+{
+	public class AddToMapPage : ContentPage
+	{
+		public AddToMapPage ()
+		{
+
+			/**
+			 * I'm just putting this here as a placeholder until we know exactly what we want.
+			 * 
+			 * Basicaly we take this information save it, then ask the user to select a locaiton, 
+			 * AND create the item at that location
+			 * 
+			 * OR.
+			 * 
+			 * We have them longPress(See MapModel) a location then have displayWindow Open with cancel or 
+			 * add to Map,i f they add to Map this window opens, we fill in data and then add the
+			 * item to the map at the known location
+			 * 
+			 * 
+			 * The second approach seems better but I'm still contemplating. 
+			 * I'm rambling now idk
+			 */
+			Label header = new Label
+			{
+				Text = "Specify Pin",
+				FontSize = 50,
+				FontAttributes = FontAttributes.Bold,
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			var unitid = new Entry { 
+				Placeholder = "Unit #",
+				VerticalOptions = LayoutOptions.Center
+			};
+			var organization = new Entry { 
+				Placeholder = "Organization",
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			//TODO is status like a dropdown? I.E. healthy/fine/good/in need of help/dieing?
+			var status = new Entry { 
+				Placeholder = "Current Status",
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			Picker picker = new Picker
+			{
+				Title = "Unit Type",
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			picker.Items.Add ("Police");
+			picker.Items.Add ("Fire");
+			picker.Items.Add ("EMS");
+			picker.Items.Add ("Volcano Response");
+			picker.Items.Add ("Hazmat");
+			picker.Items.Add ("Other");
+
+			Button addToMap = new Button
+			{
+				Text = "Add To Map",
+				BorderWidth = 1,
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.End
+			};
+
+			addToMap.Clicked += async (sender, e) => {
+				//await Navigation.PopToRootAsync();
+				//await Navigation.PopAsync(); 
+				//Use these two when we go from MapPage PopModal when coming from MapModel
+
+				await Navigation.PopModalAsync();
+			};
+
+			this.Content = new StackLayout
+			{
+				Children = 
+				{
+					header,
+					unitid,
+					organization,
+					status,
+					picker,
+					addToMap
+				}
+			};
+
+			Title = "User Profile";
+			Icon = "UserProfile.png";
+		}
+	}
+}
+
--- App/EIMA Init/EIMA/EIMA Shared/Pages/MapPage.cs
+++ App/EIMA Init/EIMA/EIMA Shared/Pages/MapPage.cs
@@ -1,53 +1,44 @@
 using System;
-using Xamarin.Forms;
 using System.Collections.Generic;
-using XLabs.Forms.Controls;
+
+using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Plugin.Geolocator;
+using XLabs.Forms.Controls;
+using TK.CustomMap;
+using TK.CustomMap.MapModel;
 
 namespace EIMAMaster
 {
 
 	public class MapPage : ContentPage
 	{
-		Map map;
+		TKCustomMap map;
 		Position defaultLocation = new Position (39.8, -84.08711552);
 		SelectMultipleBasePage<FilterModel> multiPage; //Used in Filter function
 
 		public MapPage ()
-		{
+		{			
+			startAndBindMap();
+			setToolBar ();
+	
+		}
 
-			map = new Map(
-				MapSpan.FromCenterAndRadius(defaultLocation, Distance.FromMiles(0.3))) {
-				IsShowingUser = true,
-				HeightRequest = 100,
-				WidthRequest = 960,
-				VerticalOptions = LayoutOptions.FillAndExpand
-			};
-
-			var stack = new StackLayout { Spacing = 0 };
-			stack.Children.Add(map);
-			Content = stack;
-			Title = "Maps";
-			Icon = "Map.png";
-
-			getAndSetLocation ();
-
+		public void setToolBar(){
 			/**
 			 * TOOLBAR RELATED CODE
 			 */
-
 			ToolbarItem plusTBI = null;
 			ToolbarItem refreshTBI = null;
 			ToolbarItem filterTBI = null;
 			ToolbarItem mapTypeTBI = null;
 
-			plusTBI = new ToolbarItem ("", "", () => {addToMap();}, 0, 0);
+			//plusTBI = new ToolbarItem ("", "", () => {addToMap();}, 0, 0);
 			refreshTBI = new ToolbarItem ("", "", () => {refreshData();}, 0, 0);
 			filterTBI = new ToolbarItem ("", "", () => {filterMapItems();}, 0, 0);
 			mapTypeTBI = new ToolbarItem ("", "", () => {changeMap();}, 0, 0);
 
-			plusTBI.Icon = "Plus.png";
+			//plusTBI.Icon = "Plus.png";
 			refreshTBI.Icon = "Refresh.png";
 			filterTBI.Icon = "Filter.png";
 			mapTypeTBI.Icon = "MapChange.png";
@@ -58,13 +49,47 @@ namespace EIMAMaster
 			ToolbarItems.Add (refreshTBI);
 			ToolbarItems.Add (filterTBI);
 			//+ Won't be there for netUser, is there for netMapEdit/netAdmin/stdAloneUser 
-			ToolbarItems.Add (plusTBI);
+			//ToolbarItems.Add (plusTBI);
 
 		}
 
-		public void addToMap(){
-			//https://forums.xamarin.com/discussion/39024/detect-a-tap-and-add-a-pin-to-the-map
+		public void startAndBindMap(){
+			var ms = MapSpan.FromCenterAndRadius (defaultLocation, Distance.FromMiles (0.3));
+			map = new TKCustomMap(ms);
+			map.IsShowingUser = true;
+
+			var stack = new StackLayout { Spacing = 0 };
+			stack.Children.Add(map);
+			Content = stack;
+			Title = "Maps";
+			Icon = "Map.png";
+
+			map.SetBinding(TKCustomMap.CustomPinsProperty, "Pins");
+			map.SetBinding(TKCustomMap.MapClickedCommandProperty, "MapClickedCommand");
+			map.SetBinding(TKCustomMap.MapLongPressCommandProperty, "MapLongPressCommand");
+			map.SetBinding(TKCustomMap.MapCenterProperty, "MapCenter");
+			map.SetBinding(TKCustomMap.PinSelectedCommandProperty, "PinSelectedCommand");
+			map.SetBinding(TKCustomMap.SelectedPinProperty, "SelectedPin");
+			map.SetBinding(TKCustomMap.RoutesProperty, "Routes");
+			map.SetBinding(TKCustomMap.PinDragEndCommandProperty, "DragEndCommand");
+			map.SetBinding(TKCustomMap.CirclesProperty, "Circles");
+			map.SetBinding(TKCustomMap.CalloutClickedCommandProperty, "CalloutClickedCommand");
+			map.SetBinding(TKCustomMap.PolylinesProperty, "Lines");
+			map.SetBinding(TKCustomMap.PolygonsProperty, "Polygons");
+			map.SetBinding(TKCustomMap.MapRegionProperty, "MapRegion");
+			map.SetBinding(TKCustomMap.RouteClickedCommandProperty, "RouteClickedCommand");
+			map.SetBinding(TKCustomMap.RouteCalculationFinishedCommandProperty, "RouteCalculationFinishedCommand");
+			map.SetBinding(TKCustomMap.TilesUrlOptionsProperty, "TilesUrlOptions");
+			map.AnimateMapCenterChange = true;
+		
+			this.BindingContext = new MapModel ();
+			getAndSetLocation ();
+
 		}
+
+//		public async void addToMap(){
+//			await Navigation.PushAsync (new AddToMapPage());
+//		}
 
 		public async void filterMapItems(){
 			var items = new List<FilterModel>();
@@ -84,6 +109,7 @@ namespace EIMAMaster
 		}
 
 		public void refreshData(){
+			//TODO
 			//This won't be implemented for a while.
 		}
 
