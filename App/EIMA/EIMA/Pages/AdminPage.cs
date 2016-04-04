2608a98d7400e42ac00351ee36e806ef3c7d6419
robertlaufer <robertlaufer2014@gmail.com>
2016-04-04 18:53:53 -0400

    completed admin page

--- App/EIMA/EIMA/Pages/AdminPage.cs
+++ App/EIMA/EIMA/Pages/AdminPage.cs
@@ -110,15 +110,14 @@ namespace EIMA
 					admins.Add (user);
 				}
 			}
-
+			TableRoot root = new TableRoot ();
 			var noAccess = new TableSection ("No Access");
 			foreach (EIMAUser user in noAccessUsers) {
 				var cell = new TextCell {
 					Text = user.name, 
 					TextColor = Color.White,
-					Command = new Command (async o => await Application.Current.MainPage.Navigation.PushModalAsync (new UserInfoPage (user))),
+					Command = new Command (async o => await Application.Current.MainPage.Navigation.PushModalAsync (new UserInfoPage (user)))
 				};
-
 				noAccess.Add (cell);
 			}
 
@@ -155,14 +154,115 @@ namespace EIMA
 				admin.Add (cell);
 			}
 
-			TableRoot root = new TableRoot ();
+			var refreshButton = new Button();
+
+			// big command to refresh the page with updated privilege levels
+			Command command1 = new Command (c => {
+
+				root.Remove (noAccess);
+				root.Remove (standardUser);
+				root.Remove (mapEditor);
+				root.Remove (admin);
+
+
+				foreach (EIMAUser user in Users.userList) {
+					if (noAccessUsers.Contains(user)) {
+						noAccessUsers.Remove (user);
+					}
+					else if (standardUsers.Contains(user)) {
+						standardUsers.Remove (user);
+					}
+					else if (mapEditors.Contains(user)) {
+						mapEditors.Remove (user);
+					}
+					else if (admins.Contains(user)) {
+						admins.Remove (user);
+					}
+				}
+
+				foreach (EIMAUser user in Users.userList){
+					if (user.level == "No Access") { 
+						noAccessUsers.Add(user);
+					} else if (user.level == "Standard User") { 
+						standardUsers.Add(user);
+					}	else if (user.level == "Map Editor") { 
+						mapEditors.Add(user);
+					} else if (user.level == "Admin") {
+						admins.Add(user);
+					}
+				}
+				Users.userList.Clear();
+
+				noAccess = new TableSection ("No Access");
+				foreach (EIMAUser user in noAccessUsers) {
+					var cell = new TextCell {
+						Text = user.name, 
+						TextColor = Color.White,
+						Command = new Command (async o => await Application.Current.MainPage.Navigation.PushModalAsync (new UserInfoPage (user)))
+					};
+					noAccess.Add (cell);
+				}
+
+				standardUser = new TableSection ("Standard Users");
+				foreach (EIMAUser user in standardUsers) {
+					var cell = new TextCell {
+						Text = user.name, 
+						TextColor = Color.White,
+						Command = new Command (async o => await Application.Current.MainPage.Navigation.PushModalAsync (new UserInfoPage (user))),
+					};
+
+					standardUser.Add (cell);
+				}
+
+				mapEditor = new TableSection ("Map Editors");
+				foreach (EIMAUser user in mapEditors) {
+					var cell = new TextCell {
+						Text = user.name, 
+						TextColor = Color.White,
+						Command = new Command (async o => await Application.Current.MainPage.Navigation.PushModalAsync (new UserInfoPage (user))),
+					};
+
+					mapEditor.Add (cell);
+				}
+
+				admin = new TableSection ("Admins");
+				foreach (EIMAUser user in admins) {
+					var cell = new TextCell {
+						Text = user.name, 
+						TextColor = Color.White,
+						Command = new Command (async o => await Application.Current.MainPage.Navigation.PushModalAsync (new UserInfoPage (user))),
+					};
+
+					admin.Add (cell);
+				}
+					
+				root.Add(noAccess);
+				root.Add(standardUser);
+				root.Add(mapEditor);
+				root.Add(admin);
+
+				var stackLayout2 = new StackLayout ();
+				TableView tableView2 = new TableView (root);
+				stackLayout2.Children.Add (tableView2);
+				stackLayout2.Children.Add (refreshButton);
+				Content = stackLayout2;
+			});
+
+			refreshButton = new Button {
+				Text = "Refresh",
+				HorizontalOptions = LayoutOptions.Center,
+				Command = command1,
+			};
+
 			root.Add (noAccess);
 			root.Add (standardUser);
 			root.Add (mapEditor);
 			root.Add (admin);
 			TableView tableView = new TableView (root);
-
-			Content = tableView;
+			var stackLayout = new StackLayout ();
+			stackLayout.Children.Add (tableView);
+			stackLayout.Children.Add (refreshButton);
+			Content = stackLayout;
 
 		}
 	}
