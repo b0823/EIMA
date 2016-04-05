da1a303edcf4016d874e5841c07f2264dadb50a2
robertlaufer <robertlaufer2014@gmail.com>
2016-04-05 00:36:47 -0400

    added admin guide help option

--- App/EIMA/EIMA/CONSTANTS.cs
+++ App/EIMA/EIMA/CONSTANTS.cs
@@ -1,7 +1,4 @@
 ﻿using Xamarin.Forms;
-using System;
-using System.Collections;
-using System.Linq;
 
 namespace EIMA
 {
@@ -10,25 +7,13 @@ namespace EIMA
 		//Universal constants go here. Most useful for lists used in various locations. 
 		//also keeps things fairly easy to edit from once place
 
-		public static string generateUID(){
-			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-			var random = new Random();
-
-			var sim = "-" + new string(Enumerable.Repeat(chars, 12)
-				.Select(s => s[random.Next(s.Length)]).ToArray());
-
-			var timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
-			var result = timeSpan.TotalSeconds + sim;
-
-			return result;
-		}
-
 		public static string[] helpOptions = {
 			"Section 1 - QuickStart Guide",
 			"Section 2 - Users and Login",
 			"Section 3 - Event Creation and Login",
 			"Section 4 - Map Editing",
-			"Section 5 - Messaging and Alerts"
+			"Section 5 - Messaging and Alerts",
+			"Section 6 - Administrator Guide"
 		};
 
 		public static string[] messageTypes = {
